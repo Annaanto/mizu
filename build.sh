0e30740a025da7a2506a6e8d2d99696a57a7956d
Ernest Koe <ernest.koe@proofgeist.com>
2022-05-08 14:06:05 -0400

    just fiddling around with xml and python

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 # ls -la
-if xar -h &> /dev/null
-then    
-    echo "xar could not be found"   
-else
-
+if ! xar -h &> /dev/null
+then
     xar -cf proof_mizu_theme.fmaddon proof_mizu_theme/
     echo 'build succesful'
+else
+    echo "xar could not be found"   
 fi
